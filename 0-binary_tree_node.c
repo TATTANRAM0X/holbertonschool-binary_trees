7d27f0e391431acc0141f953e7308d2be51067d9
TATTARAM0X <jhonatan.ramox@gmail.com>
2022-09-15 20:50:52 -0700

    betty style

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -13,6 +13,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *newNode = NULL;
 
 	newNode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+
 	if (!newNode)
 		return (NULL);
 
